907f374e13b9afc1da6e9985b949dffbf8e47de2
Jamie Yung <jamie.yung286@gmail.com>
2018-09-07 15:06:31 +1000

    Added SelectionFoldableWithData, and redefined SelectionFoldable in terms of it.

---
+++ src/Data/SelectionFoldableWithData.purs
@@ -0,0 +1,207 @@
+{-
+    A Foldable where at most one item is selected, and the selected item has
+    some extra data associated with it. The selected item is guaranteed to be in
+    the Foldable structure. However, no guarantees are made of its uniqueness.
+-}
+
+module Data.SelectionFoldableWithData
+    ( SelectionFoldableWithData
+    , IsSelected
+    , fromFoldable
+    , toFoldable
+    , select
+    , selectWith
+    , selectIndex
+    , selectWithIndex
+    , deselect
+    , selected
+    , selected_
+    , mapSelected
+    , foldrSelected
+    ) where
+
+import Data.Compactable (compactDefault, separateDefault)
+import Data.Either (Either(..))
+import Data.Filterable (class Compactable, class Filterable, filter, filterMap, partition, partitionMap)
+import Data.Foldable (class Foldable, foldl, foldr)
+import Data.FoldableWithIndex (class FoldableWithIndex, foldlWithIndex)
+import Data.Functor (map)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Tuple (Tuple(..), fst, snd)
+import Prelude (class Eq, class Functor, class Show, bind, show, ($), (&&), (<>), (==), (>>=))
+
+data SelectionFoldableWithData f d a
+    = Private_ (f a) (Maybe (Tuple d a))
+
+type IsSelected = Boolean
+
+instance eqSelectionFoldableWithData :: (Eq (f a), Eq d, Eq a) => Eq (SelectionFoldableWithData f d a) where
+    eq (Private_ lxs lMSel) (Private_ rxs rMSel) = (lxs == rxs) && (lMSel == rMSel)
+
+instance showSelectionFoldableWithData :: (Show (f a), Show d, Show a) => Show (SelectionFoldableWithData f d a) where
+    show (Private_ xs mSel) =
+        "(SelectionFoldableWithData " <> show xs <> " " <> show mSel <> ")"
+
+instance functorSelectionFoldableWithData :: Functor f => Functor (SelectionFoldableWithData f d) where
+    map f (Private_ xs mSel) = Private_ (map f xs) (map (map f) mSel)
+
+instance compactableSelectionFoldableWithData :: (Functor f, Compactable f) => Compactable (SelectionFoldableWithData f d) where
+    compact = compactDefault
+    separate = separateDefault
+
+instance filterableSelectionFoldable :: Filterable f => Filterable (SelectionFoldableWithData f d) where
+    partitionMap f (Private_ xs mSel) =
+        { left: Private_ lxs lMSel
+        , right: Private_ rxs rMSel
+        }
+        where
+        { left: lxs, right: _ } = partitionMap f xs
+        { left: _, right: rxs } = partitionMap f xs
+        rMSel = mSel >>= \sel -> case f (snd sel) of
+            Left _ -> Nothing
+            Right x -> Just $ Tuple (fst sel) x
+        lMSel = mSel >>= \sel -> case f (snd sel) of
+            Left x -> Just $ Tuple (fst sel) x
+            Right _ -> Nothing
+
+    partition f (Private_ xs mSel) =
+        { no: Private_ lxs lMSel
+        , yes: Private_ rxs rMSel
+        }
+        where
+        { no: lxs, yes: _ } = partition f xs
+        { no: _, yes: rxs } = partition f xs
+        lMSel = mSel >>= \sel -> if f (snd sel) then Nothing else mSel
+        rMSel = mSel >>= \sel -> if f (snd sel) then mSel else Nothing
+
+    filterMap f (Private_ xs mSel) =
+        Private_ (filterMap f xs) mSel' where
+        mSel' = do
+            sel <- mSel
+            b <- f (snd sel)
+            Just $ Tuple (fst sel) b
+
+    filter f (Private_ xs mSel) =
+        Private_ (filter f xs) (mSel >>= \sel -> if f (snd sel) then mSel else Nothing)
+
+fromFoldable :: forall f d a. Foldable f => Eq a => f a -> SelectionFoldableWithData f d a
+fromFoldable xs = Private_ xs Nothing
+
+toFoldable :: forall f d a. Foldable f => Eq a => SelectionFoldableWithData f d a -> f a
+toFoldable (Private_ xs _) = xs
+
+{-
+    Selects the first element `a` such that `a == x`.
+-}
+select :: forall f d a
+    . Foldable f
+    => Eq a
+    => d
+    -> a
+    -> SelectionFoldableWithData f d a
+    -> SelectionFoldableWithData f d a
+select d x = selectWith d (_ == x)
+
+{-
+    Selects the first element `a` such that `p a == true`.
+-}
+selectWith :: forall f d a
+    . Foldable f
+    => Eq a
+    => d
+    -> (a -> IsSelected)
+    -> SelectionFoldableWithData f d a
+    -> SelectionFoldableWithData f d a
+selectWith d p (Private_ xs curMSel) = (Private_ xs newMSel) where
+    newMSel :: Maybe (Tuple d a)
+    newMSel = case found of
+        Just x -> Just $ Tuple d x
+        Nothing -> curMSel
+
+    found :: Maybe a
+    found = foldl accFn Nothing xs
+
+    accFn :: Maybe a -> a -> Maybe a
+    accFn z x = case z of
+        Just _ -> z
+        Nothing -> if p x then Just x else Nothing
+
+selectIndex :: forall i f d a
+    . FoldableWithIndex i f
+    => Eq a
+    => Eq i
+    => d
+    -> i
+    -> SelectionFoldableWithData f d a
+    -> SelectionFoldableWithData f d a
+selectIndex d i = selectWithIndex d (\i' _ -> i == i')
+
+selectWithIndex :: forall i f d a
+    . FoldableWithIndex i f
+    => Eq a
+    => d
+    -> (i -> a -> IsSelected)
+    -> SelectionFoldableWithData f d a
+    -> SelectionFoldableWithData f d a
+selectWithIndex d p (Private_ xs curMSel) = (Private_ xs newMSel) where
+    newMSel :: Maybe (Tuple d a)
+    newMSel = case found of
+        Just (Tuple i x) -> Just (Tuple d x)
+        Nothing -> curMSel
+
+    found :: Maybe (Tuple i a)
+    found = foldlWithIndex accFn Nothing xs
+
+    accFn :: i -> Maybe (Tuple i a) -> a -> Maybe (Tuple i a)
+    accFn i z x = case z of
+        Just _ -> z
+        Nothing -> if p i x then Just (Tuple i x) else Nothing
+
+deselect :: forall f d a. SelectionFoldableWithData f d a -> SelectionFoldableWithData f d a
+deselect (Private_ xs _) = Private_ xs Nothing
+
+selected :: forall f d a. SelectionFoldableWithData f d a -> Maybe (Tuple d a)
+selected (Private_ _ mSel) = mSel
+
+selected_ :: forall f d a. SelectionFoldableWithData f d a -> Maybe a
+selected_ (Private_ _ mSel) = map snd mSel
+
+{-
+    If there exist multiple elements `a` such that `p a == true`, the function
+    will be invoked with `true` as the first argument. No guarantee of
+    uniqueness is made; that is left up to the user.
+-}
+mapSelected :: forall f d a e b
+    . Foldable f
+    => Functor f
+    => Eq a
+    => { sel :: Tuple d a -> Tuple e b, rest :: a -> b }
+    -> SelectionFoldableWithData f d a
+    -> SelectionFoldableWithData f e b
+mapSelected fns (Private_ xs mSel) = Private_ (map f xs) mSel' where
+    mSel' = map fns.sel mSel
+
+    f x =
+        if Just x == map snd mSel then
+            fromMaybe (fns.rest x) (map snd mSel')
+        else
+            fns.rest x
+
+foldrSelected :: forall f d a b
+    . Foldable f
+    => Eq a
+    => { sel :: Tuple d a -> b -> b, rest :: a -> b -> b }
+    -> b
+    -> SelectionFoldableWithData f d a
+    -> b
+foldrSelected fns b (Private_ xs mSel) = foldr accFn b xs where
+    accFn :: a -> b -> b
+    accFn x z = case mSel of
+        Nothing ->
+            fns.rest x z
+
+        Just sel ->
+            if x == snd sel then
+                fns.sel sel z
+            else
+                fns.rest x z
---
+++ test/Test/SelectionFoldableWithData.purs
@@ -0,0 +1,297 @@
+module Test.SelectionFoldableWithData where
+
+import Data.Array ((:))
+import Data.Either (Either(..))
+import Data.Filterable (filter, filterMap, partition, partitionMap)
+import Data.Maybe (Maybe(..))
+import Data.SelectionFoldableWithData (SelectionFoldableWithData)
+import Data.SelectionFoldableWithData as SFWD
+import Data.Tuple (Tuple(..))
+import Prelude (Unit, discard, map, show, unit, (#), (+), (<>), (==), (>), (>=))
+import Test.Spec (Spec, describe, it)
+import Test.Spec.Assertions (shouldEqual, shouldNotEqual)
+
+type SFArrUnitInt = SelectionFoldableWithData Array Unit Int
+
+spec :: Spec Unit
+spec = describe "SelectionFoldableWithData" do
+    describe "Eq instance" do
+        it "Structures equal, selected items equal -> equal" do
+            shouldEqual
+                (SFWD.fromFoldable [1] :: SFArrUnitInt)
+                (SFWD.fromFoldable [1] :: SFArrUnitInt)
+
+        it "Structures equal, selected items not equal -> not equal (1)" do
+            shouldNotEqual
+                (SFWD.fromFoldable [1, 2] # SFWD.select unit 1)
+                (SFWD.fromFoldable [1, 2] # SFWD.select unit 2)
+
+        it "Structures equal, selected items not equal -> not equal (2)" do
+            shouldNotEqual
+                (SFWD.fromFoldable [1, 2] # SFWD.select unit 1)
+                (SFWD.fromFoldable [1, 2]) -- Selected Nothing
+
+        it "Structures not equal, selected items equal -> not equal" do
+            shouldNotEqual
+                (SFWD.fromFoldable [1] # SFWD.select unit 1)
+                (SFWD.fromFoldable [1, 2] # SFWD.select unit 1)
+
+        it "Structures not equal, selected items not equal -> not equal (1)" do
+            shouldNotEqual
+                (SFWD.fromFoldable [1] # SFWD.select unit 1)
+                (SFWD.fromFoldable [2] # SFWD.select unit 2)
+
+        it "Structures not equal, selected items not equal -> not equal (2)" do
+            shouldNotEqual
+                (SFWD.fromFoldable [1] # SFWD.select unit 1)
+                (SFWD.fromFoldable [2]) -- Selected Nothing
+
+    describe "Functor instance" do
+        it "map works only on the structure if nothing selected" do
+            shouldEqual
+                ((SFWD.fromFoldable [1,2,3] :: SFArrUnitInt)
+                    # map (_ + 1)
+                    # show
+                )
+                ("(SelectionFoldableWithData [2,3,4] Nothing)")
+
+        it "map works on the structure and the selected item" do
+            shouldEqual
+                (SFWD.fromFoldable [1,2,3]
+                    # SFWD.select unit 1
+                    # map (_ + 1)
+                    # show
+                )
+                ("(SelectionFoldableWithData [2,3,4] (Just (Tuple unit 2)))")
+
+    describe "Filterable instance" do
+        it "partitionMap" do
+            shouldEqual
+                ((SFWD.fromFoldable [1, 2, 3] :: SFArrUnitInt)
+                    # partitionMap \n ->
+                        if n >= 2 then Left (show n) else Right (show n))
+                ({ left: SFWD.fromFoldable ["2", "3"]
+                , right: SFWD.fromFoldable ["1"]
+                })
+
+        it "partition" do
+            shouldEqual
+                ((SFWD.fromFoldable [1, 2, 3] :: SFArrUnitInt)
+                # partition (_ >= 2))
+                { yes: SFWD.fromFoldable [2, 3]
+                , no: SFWD.fromFoldable [1]
+                }
+
+        it "filterMap works only on the structure if nothing selected" do
+            shouldEqual
+                ((SFWD.fromFoldable [1, 2, 3] :: SFArrUnitInt)
+                    # filterMap \n -> if n > 1 then Just (show n) else Nothing)
+                (SFWD.fromFoldable ["2", "3"])
+
+        it "filterMap works on the selected item" do
+            shouldEqual
+                (SFWD.fromFoldable [1, 2, 3]
+                    # SFWD.select unit 2
+                    # filterMap (\n -> if n > 1 then Just (show n) else Nothing)
+                    # show)
+                ("(SelectionFoldableWithData [\"2\",\"3\"] (Just (Tuple unit \"2\")))")
+
+        it "filter works only on the structure if nothing selected" do
+            shouldEqual
+                ((SFWD.fromFoldable [1, 2, 3] :: SFArrUnitInt)
+                    # filter (_ > 1))
+                (SFWD.fromFoldable [2, 3])
+
+        it "filter retains the selected item if p(selected) == true" do
+            shouldEqual
+                (SFWD.fromFoldable [1, 2, 3]
+                    # SFWD.select unit 2
+                    # filter (_ > 1)
+                    # show)
+                ("(SelectionFoldableWithData [2,3] (Just (Tuple unit 2)))")
+
+        it "filter removes the selected item if p(selected) == false" do
+            shouldEqual
+                (SFWD.fromFoldable [1, 2, 3]
+                    # SFWD.select unit 1
+                    # filter (_ > 1)
+                    # show)
+                ("(SelectionFoldableWithData [2,3] Nothing)")
+
+    describe "fromFoldable" do
+        it "fromFoldable" do
+            shouldEqual
+                ((SFWD.fromFoldable [1] :: SFArrUnitInt) # show)
+                ("(SelectionFoldableWithData [1] Nothing)")
+
+    describe "toFoldable" do
+        it "toFoldable" do
+            shouldEqual
+                (SFWD.fromFoldable [1] # SFWD.toFoldable)
+                ([1])
+
+    describe "select" do
+        it "match" do
+            shouldEqual
+                (SFWD.fromFoldable [1] # SFWD.select unit 1 # show)
+                ("(SelectionFoldableWithData [1] (Just (Tuple unit 1)))")
+
+        it "no match" do
+            shouldEqual
+                (SFWD.fromFoldable [1] # SFWD.select unit 2 # show)
+                ("(SelectionFoldableWithData [1] Nothing)")
+
+        it "select overrides any previous selection" do
+            shouldEqual
+                (SFWD.fromFoldable [1, 2] # SFWD.select unit 1 # SFWD.select unit 2 # show)
+                ("(SelectionFoldableWithData [1,2] (Just (Tuple unit 2)))")
+
+        it "attempting to select an element not found in the structure does nothing" do
+            shouldEqual
+                ((SFWD.fromFoldable [1, 2] :: SFArrUnitInt)
+                    # SFWD.select unit 1 # SFWD.select unit 5 # show)
+                ("(SelectionFoldableWithData [1,2] (Just (Tuple unit 1)))")
+
+    describe "selectWith" do
+        it "match" do
+            shouldEqual
+                (SFWD.fromFoldable [1] # SFWD.selectWith unit (_ == 1) # show)
+                ("(SelectionFoldableWithData [1] (Just (Tuple unit 1)))")
+
+        it "no match" do
+            shouldEqual
+                (SFWD.fromFoldable [1] # SFWD.selectWith unit (_ == 2) # show)
+                ("(SelectionFoldableWithData [1] Nothing)")
+
+        it "selects the first match" do
+            shouldEqual
+                (SFWD.fromFoldable [1, 2, 3] # SFWD.selectWith unit (_ >= 2) # show)
+                ("(SelectionFoldableWithData [1,2,3] (Just (Tuple unit 2)))")
+
+    describe "selectIndex" do
+        it "match" do
+            shouldEqual
+                (SFWD.fromFoldable [1] # SFWD.selectIndex unit 0 # show)
+                ("(SelectionFoldableWithData [1] (Just (Tuple unit 1)))")
+
+        it "no match" do
+            shouldEqual
+                (SFWD.fromFoldable [1] # SFWD.selectIndex unit 1 # show)
+                ("(SelectionFoldableWithData [1] Nothing)")
+
+        it "select overrides any previous selection" do
+            shouldEqual
+                (SFWD.fromFoldable [1, 2] # SFWD.selectIndex unit 0 # SFWD.selectIndex unit 1 # show)
+                ("(SelectionFoldableWithData [1,2] (Just (Tuple unit 2)))")
+
+        it "attempting to select an element not found in the structure does nothing" do
+            shouldEqual
+                (SFWD.fromFoldable [1, 2] # SFWD.selectIndex unit 0 # SFWD.selectIndex unit 5 # show)
+                ("(SelectionFoldableWithData [1,2] (Just (Tuple unit 1)))")
+
+    describe "selectWithIndex" do
+        it "match index" do
+            shouldEqual
+                (SFWD.fromFoldable [1] # SFWD.selectWithIndex unit (\i _ -> i == 0) # show)
+                ("(SelectionFoldableWithData [1] (Just (Tuple unit 1)))")
+
+        it "match element" do
+            shouldEqual
+                (SFWD.fromFoldable [1] # SFWD.selectWithIndex unit (\_ x -> x == 1) # show)
+                ("(SelectionFoldableWithData [1] (Just (Tuple unit 1)))")
+
+        it "no match index" do
+            shouldEqual
+                (SFWD.fromFoldable [1] # SFWD.selectWithIndex unit (\i _ -> i == 2) # show)
+                ("(SelectionFoldableWithData [1] Nothing)")
+
+        it "no match element" do
+            shouldEqual
+                (SFWD.fromFoldable [1] # SFWD.selectWithIndex unit (\_ x -> x == 2) # show)
+                ("(SelectionFoldableWithData [1] Nothing)")
+
+        it "selects the first match" do
+            shouldEqual
+                (SFWD.fromFoldable [1, 2, 3] # SFWD.selectWithIndex unit (\i _ -> i >= 1) # show)
+                ("(SelectionFoldableWithData [1,2,3] (Just (Tuple unit 2)))")
+
+    describe "deselect" do
+        it "does nothing if nothing selected" do
+            shouldEqual
+                ((SFWD.fromFoldable [1] :: SFArrUnitInt) # SFWD.deselect)
+                ((SFWD.fromFoldable [1] :: SFArrUnitInt))
+
+        it "deselects the selected item" do
+            shouldEqual
+                (SFWD.fromFoldable [1] # SFWD.select unit 1 # SFWD.deselect)
+                (SFWD.fromFoldable [1])
+
+    describe "selected" do
+        it "returns Nothing if nothing selected" do
+            shouldEqual
+                ((SFWD.fromFoldable [1] :: SFArrUnitInt) # SFWD.selected)
+                (Nothing)
+
+        it "returns the selected item" do
+            shouldEqual
+                ((SFWD.fromFoldable [1] :: SFArrUnitInt) # SFWD.select unit 1 # SFWD.selected)
+                (Just (Tuple unit 1))
+
+    describe "selected_" do
+        it "returns Nothing if nothing selected" do
+            shouldEqual
+                ((SFWD.fromFoldable [1] :: SFArrUnitInt) # SFWD.selected_)
+                (Nothing)
+
+        it "returns the selected item" do
+            shouldEqual
+                ((SFWD.fromFoldable [1] :: SFArrUnitInt) # SFWD.select unit 1 # SFWD.selected_)
+                (Just 1)
+
+    describe "mapSelected" do
+        it "works only on the structure if nothing selected" do
+            shouldEqual
+                ((SFWD.fromFoldable [1,2,3] :: SFArrUnitInt)
+                    # SFWD.mapSelected
+                        { sel: \(Tuple d n) -> Tuple d (n + 10)
+                        , rest: \n -> n + 1
+                        }
+                    # show
+                )
+                ("(SelectionFoldableWithData [2,3,4] Nothing)")
+
+        it "applies the selected fn to the selected item" do
+            shouldEqual
+                ((SFWD.fromFoldable [1,2,3] :: SFArrUnitInt)
+                    # SFWD.select unit 1
+                    # SFWD.mapSelected
+                        { sel: \(Tuple d n) -> Tuple d (n + 10)
+                        , rest: \n -> n + 1
+                        }
+                    # show
+                )
+                ("(SelectionFoldableWithData [11,3,4] (Just (Tuple unit 11)))")
+
+        it "applies the selected fn to all items equal to the selected item" do
+            shouldEqual
+                (SFWD.fromFoldable [1,1,1]
+                    # SFWD.select unit 1
+                    # SFWD.mapSelected
+                        { sel: \(Tuple d n) -> Tuple d (n + 10)
+                        , rest: \n -> n + 1
+                        }
+                    # show
+                )
+                ("(SelectionFoldableWithData [11,11,11] (Just (Tuple unit 11)))")
+
+    describe "foldrSelected" do
+        it "foldrSelected" do
+            shouldEqual
+                (SFWD.fromFoldable [1,2,3]
+                    # SFWD.select "!" 1
+                    # SFWD.foldrSelected
+                        { sel: \(Tuple s x) z -> (show x <> s) : z
+                        , rest: \x z -> (show x) : z
+                        } []
+                )
+                (["1!","2","3"])
